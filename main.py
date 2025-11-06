7b1e1c85c4b9303af09b7f3a16ce460c53f66836
raphael-charlerydelamasseliere <raphael.charlerydelamasseliere@edu.esiee.fr>
2025-11-06 09:35:36 +0000

    Mon programme pour vérifier si un nombre est premier

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
+"""
+Ce module contient la fonction isprime qui vérifie si un nombre est premier
+"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
-
+    """
+    Retourne la valeur de vérité de la proposition : p est-il premier ?
+    Args:
+        p: un potentiel nombre premier
+    Returns:
+        True or False 
+    """
     # votre code ici
+    premier = True
 
-    pass
+    if p == 1:
+        premier = False
+        return premier
 
+    for i in range(2, int(sqrt(p))+1):
+        if p % i == 0:
+            premier = False
+            return premier
+    return premier
 #### Fonction principale
 
-
 def main():
-
+    """
+    Réalise les tests pour vérifier des nombres premiers
+    """
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
@@ -21,6 +39,10 @@ def main():
             print(n, end=", ")
 
     print()
+
+    print(isprime(1))
+    print(isprime(13))
+    print(isprime(20))
 
 
 if __name__ == "__main__":
